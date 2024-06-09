73891baeb01951a08c440d2f15a920e170e4a6de
ydj515 <ydj515@hanmail.net>
2024-06-09 13:32:14 +0900

    chore: 불필요 주석 제거

--- app/database.py
+++ app/database.py
@@ -3,14 +3,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# settings.json 파일 경로
 SETTINGS_FILE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'settings.json')
 
-# JSON 파일 읽기
 with open(SETTINGS_FILE_PATH, 'r') as file:
     settings = json.load(file)
 
-# 설정 값을 사용하여 SQLAlchemy 데이터베이스 URL 구성
 db_settings = settings['database']
 SQLALCHEMY_DATABASE_URL = (
     f"mysql+mysqlconnector://{db_settings['username']}:{db_settings['password']}@"
